867decef9ed7aad5bc1374950257515d3dda4e47
tarena <1634137072@qq>
2022-06-15 19:48:47 +0800

    github测试

---
+++ src/test/java/cn/tedu/github_test/GithubTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.github_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
